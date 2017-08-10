44ef41cc056afdce85b4001f77530a06e4f3a514
sujithbabu1971 <sujithkp@yahoo.com>
2017-08-10 15:59:14 +0530

    Connectig local application with remote database in MLabs cloud

--- mongooseaddrecordwithvalidation.js
+++ mongooseaddrecordwithvalidation.js
@@ -8,10 +8,13 @@ var cricketScoreModel = mongoose.model('cricketScoreModel',cricketScoreSchema);
 cricketScoreModel.schema.path("runs").validate(function (val){
 	return val>=0;
 }, "Runs can not be less than zero");
-mongoose.connect("mongodb://localhost/cricket");
+var dbString ="mongodb://sujithuser:sujithpassword@ds157500.mlab.com:57500/sujith";
+//var dbString ="mongodb://localhost/cricket";
+
+mongoose.connect(dbString);
 mongoose.connection.once('open', function(){
 	
-	cricketScoreModel.create({"batsman":"Martin Crowe","runs":-2}, function (err, removedReds){
+	cricketScoreModel.create({"batsman":"Sunil gavaskar","runs":122}, function (err, removedReds){
 		if(err)
 		{
 			console.log(" Could not insert. Reason -->"+err.errors.runs.message);
